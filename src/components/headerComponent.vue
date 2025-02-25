6bbc0397896283d951d79307ef7e5b0922985c1a
= <=>
2025-02-25 22:28:51 +0500

    feat: header

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -3,9 +3,32 @@
 </script>
 
 <template>
-
+    <div class="container">
+        <div class="header flex">
+            <img src="@assets/imgs/logo.svg" alt="">
+            <nav>
+                <a href="">Описание</a>
+                <a href="">Характеристики</a>
+                <button>стоимость</button>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .header {
+        width: 100%;
+    }
 
+    .header nav {
+        max-width: 425px;
+        gap: 10px;
+    }
+
+    .header nav button {
+        padding: 18px 40px;
+
+        font-size: .8rem;
+        letter-spacing: normal;
+    }
 </style>
